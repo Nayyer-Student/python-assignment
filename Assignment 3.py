71ebfc57cf9f527c7f8b5595121b753c8c779116
Nayyerart <109219113+Nayyerart@users.noreply.github.com>
2022-07-14 22:32:04 +0500

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,25 @@
+mark1 = int(input("Enter your English Marks:  "))
+mark2 = int(input("Enter your Urdu Marks: "))
+mark3 = int(input("Enter your Maths Marks: "))
+mark4 = int(input("Enter your Physics Marks: "))
+mark5 = int(input("Enter your Chemistry Marks: "))
+
+print("****Mark Sheet******")
+print("Subject             Marks         Obtained Marks")
+
+total = mark1+mark2+mark3+mark4+mark5
+percent = (total * 100)/500
+
+print("English              100            ", mark1)
+print("Urdu                 100            ", mark2)
+print("Maths                100            ", mark3)
+print("Physics              100            ", mark4)
+print("Chemistry            100            ", mark5)
+print("Total Marks: 500")
+print("Obtained Marks", total)
+print("Percentage: ", percent)
+
+if(percent > 40):
+    print("Pass")
+else:
+    print("Fail")
